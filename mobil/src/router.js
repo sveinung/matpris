b6303b9c0fd25c2f2916b96b4f749b60674a09ce
Sveinung Dalatun <sdalatun@gmail.com>
2017-05-25 10:15:18 +0200

    Legg til tannhjul

--- mobil/src/router.js
+++ mobil/src/router.js
@@ -9,10 +9,13 @@ import {
   Scene,
   Actions,
 } from 'react-native-router-flux';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 import Handleliste from './components/handleliste';
 import Logginn from './components/logginn';
 import RegistrerBrukar from './components/registrerBrukar';
+
+const DODGER_BLUE = '#1e90ff';
 
 const RouterComponent = () => (
   <View style={{ flex: 1 }}>
@@ -25,20 +28,28 @@ const RouterComponent = () => (
           component={Logginn}
           title="Logg inn"
           initial={true}
+          onRight={() => {}}
+          renderRightButton={renderRightButton}
         />
         <Scene
           key="handleliste"
           component={Handleliste}
           title="Legg til vare"
+          onRight={() => {}}
+          renderRightButton={renderRightButton}
         />
         <Scene
           key="registrerBrukar"
           component={RegistrerBrukar}
           title="Register brukar"
+          onRight={() => {}}
+          renderRightButton={renderRightButton}
         />
       </Scene>
     </Router>
   </View>
 );
+
+const renderRightButton = () => <Icon name="gear" size={30} color={DODGER_BLUE} />;
 
 export default RouterComponent;
